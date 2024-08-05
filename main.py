d2ea1edab85b6118b15ea3bc15c7d700b0236f30
isuruthiwa <isuruthiwa@gmail.com>
2024-08-05 19:20:02 +0530

    Add YoLov8 model and SAM model connection to determine plant leaf area

--- main.py
+++ main.py
@@ -1,56 +1,5 @@
-import os
-import cv2
-import torch
-import matplotlib.pyplot as plt
-import numpy as np
-from sam2.build_sam import build_sam2
-from sam2.sam2_image_predictor import SAM2ImagePredictor
-from ultralytics import YOLO
-
-from utils.helper_functions import show_mask
-
-# Checkpoint paths and model type definitions
-DEVICE = "cuda"
-SAM2_CHECKPOINT_PATH = "utils/SAM_Model_Weights/SAM2/sam2_hiera_tiny.pt"
-MODEL_CFG = "sam2_hiera_t.yaml"
-YOLO_MODEL_CHECKPOINT = "train_yolo_model/model_runs/results/weights/best.pt"
-
-
-# use bfloat16 for the run
-torch.autocast(device_type=DEVICE, dtype=torch.bfloat16).__enter__()
-
-
-# Specify the output directory and file name
-output_dir = 'output_results'
-file_name = 'plot_image.png'
-os.makedirs(output_dir, exist_ok=True)
-
-model = YOLO(YOLO_MODEL_CHECKPOINT)
+from segment_leaf_area_model import SegmentLeafAreaUsingYoloSAM2
 
 image_path = "/home/isuruthiwa/Research/LAI/Dataset/plant_images/train_images/Plant8_front_Color.png"
-results = model(image_path)
-
-image = cv2.imread(image_path)
-bounding_box = np.array(results[0].boxes[0].xyxy[0].cpu())
-
-sam2_model = build_sam2(MODEL_CFG, SAM2_CHECKPOINT_PATH, device=DEVICE)
-predictor = SAM2ImagePredictor(sam2_model)
-
-predictor.set_image(image)
-masks, scores, _ = predictor.predict(
-    point_coords=None,
-    point_labels=None,
-    box=bounding_box,
-    multimask_output=False)
-
-best_mask = masks[scores.argmax()]
-best_score = scores[scores.argmax()]
-
-plt.imshow(image)
-show_mask(best_mask, plt.gca())
-plt.title(f"Mask, Score: {best_score:.3f}", fontsize=18)
-
-# Save the plot as an image
-plt.savefig(f'{output_dir}/{file_name}', format='png', dpi=300)
-
-plt.show()
+segment_model = SegmentLeafAreaUsingYoloSAM2()
+mask, score = segment_model.predict(image_path)
--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -1,16 +1,22 @@
 # helper_functions.py
 
-import os
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
 
-def show_mask(mask, ax, random_color=False):
+def show_mask(image, mask, score, output_dir, file_name, random_color=False):
+    plt.imshow(image)
     if random_color:
         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
     else:
         color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
     h, w = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-    ax.imshow(mask_image)
+    plt.gca().imshow(mask_image)
+
+    plt.title(f"Mask, Score: {score:.3f}", fontsize=18)
+
+    # Save the plot as an image
+    plt.savefig(f'{output_dir}/{file_name}', format='png', dpi=300)
+
+    plt.show()
